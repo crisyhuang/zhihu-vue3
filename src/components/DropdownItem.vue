62b6aaca10966661092283ec4ce51cf45ddbaae1
Crisyhuang <1205281078@qq.com>
2021-08-23 22:46:33 +0800

    feature: dropdown组件-添加dropdownItem，提升可定制性及语义化

---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li
+    class="dropdown-option"
+    :class="{ 'is-disabled': disabled }"
+  >
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DropdownItem',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+
+</script>
+
+<style>
+.dropdown-option.is-disabled * {
+  color: #999;
+  background: transparent;
+  pointer-events: none;
+}
+</style>
